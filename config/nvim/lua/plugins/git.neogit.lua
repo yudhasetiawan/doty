b2a6cc7b5403921ac6ed985d6d3b127788ad645e
yudha-ulysses <yudha@ulyssestwo.com>
2025-12-20 03:25:07 +0700

    chore: disabled neogit

--- config/nvim/lua/plugins/git.neogit.lua
+++ config/nvim/lua/plugins/git.neogit.lua
@@ -2,8 +2,9 @@ return {
   {
     -- Git
     "NeogitOrg/neogit",
+    enabled = false,
     dependencies = {
-      "nvim-lua/plenary.nvim", -- required
+      "nvim-lua/plenary.nvim",  -- required
       "sindrets/diffview.nvim", -- optional - Diff integration
 
       -- Only one of these is needed, not both.
